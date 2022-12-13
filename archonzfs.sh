882cf42884215b72f8a65777c7015a1a531be9bc
Mark Mihalik <markamihalik@gmail.com>
2022-12-13 15:04:55 -0500

    mkinitpio hook, remove unencrypted dataset

--- archonzfs.sh
+++ archonzfs.sh
@@ -40,10 +40,17 @@ ask "Do you want to repartition $DISK?"
 
 # Set ZFS passphrase
 print "Set ZFS passphrase for Encrypted Datasets"
-read -r -p "> ZFS passphrase: " -s pass
-echo
-echo "$pass" > /etc/zfs/zroot.key
-chmod 0600 /etc/zfs/zroot.key
+while true; do
+  read -s -p "ZFS passphrase: " pass1
+  echo
+  read -s -p "Verify ZFS passphrase: " pass2
+  echo
+  [ "$pass1" = "$pass2" ] && break || echo "Oops, please try again"
+done
+echo "$pass2" > /etc/zfs/zroot.key
+chmod 000 /etc/zfs/zroot.key
+unset pass1
+unset pass2
 
 # Create ZFS pool
 print "Create ZFS Pool"
@@ -59,30 +66,25 @@ zpool create -f -o ashift=12              \
                 -O devices=off            \
                 -R /mnt                   \
                 -O compression=lz4        \
+                -O encryption=aes-256-gcm \
+                -O keyformat=passphrase   \
+                -O keylocation=file:///etc/zfs/zroot.key     \
                 zroot "$ZFS"
-# Create Encrypted and Unencrypted Datasets
-print "Create Encrypted Dataset"
-zfs create  -o mountpoint=none                       \
-            -o keyformat=passphrase                  \
-            -o keylocation=file:///etc/zfs/zroot.key zroot/enc
-
-print "Create Unencrypted Dataset"
-zfs create -o mountpoint=none zroot/pt
 
 # Build Dataset Tree
 print "Create Root Dataset"
-zfs create -o mountpoint=none zroot/enc/ROOT
-zfs create -o mountpoint=/ -o canmount=noauto zroot/enc/ROOT/default
+zfs create -o mountpoint=none zroot/ROOT
+zfs create -o mountpoint=/ -o canmount=noauto zroot/ROOT/default
 print "Create Data Datasets"
-zfs create -o mountpoint=none zroot/enc/data
-zfs create -o mountpoint=/home zroot/enc/data/home
-zfs create -o mountpoint=/root zroot/enc/data/home/root
+zfs create -o mountpoint=none zroot/data
+zfs create -o mountpoint=/home zroot/data/home
+zfs create -o mountpoint=/root zroot/data/home/root
 print "Create System Datasets"
-zfs create -o mountpoint=/var -o canmount=off zroot/enc/var
-zfs create zroot/enc/var/log
-zfs create -o mountpoint=/var/lib -o canmount=off zroot/enc/var/lib
-zfs create zroot/enc/var/lib/libvirt
-zfs create zroot/enc/var/lib/lxd
+zfs create -o mountpoint=/var -o canmount=off zroot/var
+zfs create zroot/var/log
+zfs create -o mountpoint=/var/lib -o canmount=off zroot/var/lib
+zfs create zroot/var/lib/libvirt
+zfs create zroot/var/lib/lxd
 
 # Export and Reimport Pools
 print "Export and Reimport Pools, mount partitions"
@@ -90,7 +92,7 @@ zpool export zroot
 zpool import -d /dev/disk/by-id -R /mnt zroot -N
 
 zfs load-key zroot
-zfs mount zroot/enc/ROOT/default
+zfs mount zroot/ROOT/default
 zfs mount -a
 mkdir -p /mnt/efi
 mount "$EFI" /mnt/efi
@@ -102,7 +104,7 @@ zgenhostid
 
 #Set Bootfs
 print "Set ZFS bootfs"
-zpool set bootfs=zroot/enc/ROOT/default zroot
+zpool set bootfs=zroot/ROOT/default zroot
 
 #Zpool Cache
 print "Create zpool cachefile"
@@ -111,7 +113,7 @@ zpool set cachefile=/etc/zfs/zpool.cache zroot
 cp /etc/zfs/zpool.cache /mnt/etc/zfs/
 
 # System Install
-root_dataset=zroot/enc/ROOT/default
+root_dataset=zroot/ROOT/default
 
 # Sort Mirrors
 print "Sorting Fastest Mirrors in US"
@@ -157,6 +159,8 @@ sed -i 's/#\(en_US.UTF-8\)/\1/' /mnt/etc/locale.gen
 # mkinitcpio
 print "Barebones mkinitcpio uefi configuration"
 sed -i 's/HOOKS=/#HOOKS=/' /mnt/etc/mkinitcpio.conf
+sed -i 's/FILES=/#FILES=/' /mnt/etc/mkinitcpio.conf
+echo "FILES=(/keys/secret.jwe)"
 echo "HOOKS=(base udev autodetect modconf kms keyboard block zfs filesystems)" >> /mnt/etc/mkinitcpio.conf
 
 cat > /mnt/etc/mkinitcpio.d/linux-lts.preset <<"EOF"
@@ -196,6 +200,11 @@ Server = https://zxcvfdsa.com/archzfs/$repo/$arch
 Server = http://archzfs.com/$repo/$arch
 EOF
 
+# Clevis TPM unlock preparation
+cp /etc/zfs/zroot.key /mnt/tmp/
+mkdir /mnt/keys
+
+
 # Chroot!
 print "Chroot into System"
 arch-chroot /mnt /bin/bash -xe << EOF
@@ -205,6 +214,7 @@ pacman -Syu --noconfirm zfs-dkms zfs-utils
 ln -sf /usr/share/zoneinfo/US/Eastern /etc/localtime
 hwclock --systohc
 locale-gen
+clevis-encrypt-tpm2 '{}' < /tmp/zroot.key > /keys/secret.jwe
 mkinitcpio -P
 bootctl install
 mkdir -p /etc/zfs/zfs-list.cache
